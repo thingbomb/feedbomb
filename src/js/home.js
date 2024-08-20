bc2ab069a75a3e04cf1f66431b29f07f4d1285e7
George Stone <georgestone@Mac.lan>
2024-08-19 20:11:50 -0400

    Fixed layout on "Add feed" button

--- src/js/home.js
+++ src/js/home.js
@@ -151,7 +151,7 @@ async function getSavedFeeds() {
 
         if (!document.querySelector(".feed-item.add-feed")) {
             let addFeedButton = document.createElement("button");
-            addFeedButton.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#e8eaed"><path d="M440-440H240q-17 0-28.5-11.5T200-480q0-17 11.5-28.5T240-520h200v-200q0-17 11.5-28.5T480-760q17 0 28.5 11.5T520-720v200h200q17 0 28.5 11.5T760-480q0 17-11.5 28.5T720-440H520v200q0 17-11.5 28.5T480-200q-17 0-28.5-11.5T440-240v-200Z"/></svg><span>Add feed</span>`;
+            addFeedButton.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#e8eaed"><path d="M440-440H240q-17 0-28.5-11.5T200-480q0-17 11.5-28.5T240-520h200v-200q0-17 11.5-28.5T480-760q17 0 28.5 11.5T520-720v200h200q17 0 28.5 11.5T760-480q0 17-11.5 28.5T720-440H520v200q0 17-11.5 28.5T480-200q-17 0-28.5-11.5T440-240v-200Z"/></svg><div class="spacer"></div><span>Add feed</span>`;
             addFeedButton.className = "feed-item add-feed";
             addFeedButton.addEventListener("click", addFeed);
             document.querySelector(".feed-list").appendChild(addFeedButton);
