e727a2ca5c70d2de5584744d56f43a6899bf15a7
georg-stone <george@thingbomb.com>
2024-10-03 16:59:07 -0400

    fix issue #3

--- src/app/api/fetchFeed/route.js
+++ src/app/api/fetchFeed/route.js
@@ -1,33 +1,47 @@
-// route.js
+import jschardet from "jschardet";
+import iconv from "iconv-lite";
 
 export async function GET(req) {
   const { searchParams } = new URL(req.url);
   const url = searchParams.get("url");
 
   if (!url) {
-    return new Response('Missing "url" parameter', { status: 400 });
+    return new Response("Request missing required parameter 'url'.", {
+      status: 400,
+    });
   }
 
   if (url.endsWith("/api/fetchFeed")) {
-    return new Response("nice try", { status: 400 });
+    return new Response("You can't recursively call this endpoint.", {
+      status: 400,
+    });
   }
 
   try {
     const response = await fetch(url);
 
     if (!response.ok) {
-      return new Response("Failed to fetch the URL", {
+      return new Response("Failed to fetch the feed.", {
         status: response.status,
       });
     }
 
-    const text = await response.text();
-    return new Response(text, {
+    const buffer = await response.arrayBuffer();
+
+    const detectedEncoding = jschardet.detect(Buffer.from(buffer));
+    let encoding = detectedEncoding.encoding || "utf-8";
+
+    const decodedText = iconv.decode(Buffer.from(buffer), encoding);
+
+    return new Response(decodedText, {
       headers: {
-        "Content-Type": response.headers.get("Content-Type"),
+        "Content-Type": response.headers.get("Content-Type") || "text/plain",
       },
     });
   } catch (error) {
-    return new Response("Error fetching the URL", { status: 500 });
+    console.error("There was an error while fetching this feed: ", error);
+    return new Response("There was an error while fetching this feed", {
+      status: 500,
+    });
   }
 }
