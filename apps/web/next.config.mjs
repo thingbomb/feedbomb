8e9e95934ef459349b23f650ffc9ac365e1e5f12
georg-stone <george@thingbomb.com>
2024-11-15 19:17:05 -0500

    fix build error

--- apps/web/next.config.mjs
+++ apps/web/next.config.mjs
@@ -1,6 +1,13 @@
 /** @type {import('next').NextConfig} */
+
+import { setupDevPlatform } from "@cloudflare/next-on-pages/next-dev";
+
 const nextConfig = {
   reactStrictMode: false,
 };
+
+if (process.env.NODE_ENV === "development") {
+  await setupDevPlatform();
+}
 
 export default nextConfig;
